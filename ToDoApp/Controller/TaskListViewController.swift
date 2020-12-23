93626b6ca1d626b903cfc36df735eff3737aec54
vesnna <vesna.nikita@yandex.ru>
2020-12-23 23:30:48 +0300

    test TaskManager sharing

--- ToDoApp/Controller/TaskListViewController.swift
+++ ToDoApp/Controller/TaskListViewController.swift
@@ -14,13 +14,15 @@ class TaskListViewController: UIViewController {
     @IBOutlet var dataProvider: DataProvider!
     @IBAction func addNewTask(_ sender: UIBarButtonItem) {
         if let viewController = storyboard?.instantiateViewController(withIdentifier: String(describing: NewTaskViewController.self)) as? NewTaskViewController {
+            viewController.taskManager = self.dataProvider.taskManager
             present(viewController, animated: true, completion: nil)
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let taskManager = TaskManager()
+        dataProvider.taskManager = taskManager
     }
 
 }
--- ToDoAppTests/Controller/TaskListViewControllerTests.swift
+++ ToDoAppTests/Controller/TaskListViewControllerTests.swift
@@ -51,20 +51,29 @@ class TaskListViewControllerTests: XCTestCase {
         XCTAssertEqual(targer as? TaskListViewController, sut)
     }
     
-    func testAddNewTaskPresentsNewTaskViewController() {
-        XCTAssertNil(sut.presentedViewController)
+    func presentingNewTaskViewController() -> NewTaskViewController {
         guard let newTaskButton = sut.navigationItem.rightBarButtonItem,
               let action = newTaskButton.action else {
-            XCTFail()
-            return
+            return NewTaskViewController()
         }
         
         UIApplication.shared.keyWindow?.rootViewController = sut
         sut.performSelector(onMainThread: action, with: newTaskButton, waitUntilDone: true)
-        XCTAssertNotNil(sut.presentedViewController)
-        XCTAssertTrue(sut.presentedViewController is NewTaskViewController)
         
         let newTaskViewController = sut.presentedViewController as! NewTaskViewController
+        return newTaskViewController
+    }
+    
+    func testAddNewTaskPresentsNewTaskViewController() {
+        let newTaskViewController = presentingNewTaskViewController()
         XCTAssertNotNil(newTaskViewController.titleTextField)
+    }
+    
+    func testSharesSameTaskManagerWithNewTaskVC() {
+        let newTaskViewController = presentingNewTaskViewController()
+        
+        XCTAssertNotNil(sut.dataProvider.taskManager)
+        XCTAssertNotNil(newTaskViewController.taskManager)
+        XCTAssertTrue(newTaskViewController.taskManager === sut.dataProvider.taskManager)
     }
 }

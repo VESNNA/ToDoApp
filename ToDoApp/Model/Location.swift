bb890ca7419bdc3ffe758d027d434785a3f6b2e7
vesnna <vesna.nikita@yandex.ru>
2020-12-25 21:38:43 +0300

    test Task model pre-serialization

--- ToDoApp/Model/Location.swift
+++ ToDoApp/Model/Location.swift
@@ -21,6 +21,21 @@ struct Location {
     
 }
 
+extension Location {
+    typealias PlistDict = [String : Any]
+    init?(dict: PlistDict) {
+        self.name = dict["name"] as! String
+        if let latitude = dict["latitude"] as? Double,
+           let longitude = dict["longitude"] as? Double {
+            self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        } else {
+            self.coordinate = nil
+        }
+        
+        
+    }
+}
+
 extension Location: Equatable {
     static func == (lhs: Location, rhs: Location) -> Bool {
         guard rhs.coordinate?.latitude == lhs.coordinate?.latitude && rhs.coordinate?.longitude == lhs.coordinate?.longitude && rhs.name == rhs.name else { return false }
--- ToDoAppTests/Model/TaskTests.swift
+++ ToDoAppTests/Model/TaskTests.swift
@@ -50,4 +50,19 @@ class TaskTests: XCTestCase {
         
         XCTAssertEqual(location, task.location)
     }
+    
+    func testCanBeCreatedFromPlistDictionary() {
+        let date = Date(timeIntervalSince1970: 10)
+        let location = Location(name: "Baz")
+        let task = Task(title: "Foo", description: "Bar", date: date, location: location)
+        
+        let locationDictionary: [String : Any] = ["name" : "Baz"]
+        let dictionary: [String : Any] = ["title" : "Foo",
+                                          "description" : "Bar",
+                                          "date" : date,
+                                          "location" : locationDictionary]
+        let createdTask = Task(dict: dictionary)
+        
+        XCTAssertEqual(task, createdTask)
+    }
 }

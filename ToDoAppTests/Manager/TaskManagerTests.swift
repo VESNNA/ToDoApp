ee6606cd816159656c2e33a12ebcb9ae9dc9f464
vesnna <vesna.nikita@yandex.ru>
2020-12-26 19:46:15 +0300

    test save tasks

--- ToDoAppTests/Manager/TaskManagerTests.swift
+++ ToDoAppTests/Manager/TaskManagerTests.swift
@@ -14,11 +14,14 @@ class TaskManagerTests: XCTestCase {
     var sut: TaskManager!
     
     override func setUpWithError() throws {
+        super.setUp()
         sut = TaskManager()
     }
 
     override func tearDownWithError() throws {
+        sut.removeAll()
         sut = nil
+        super.tearDown()
     }
 
     func testInitTaskManagerWithZeroTasks() {
@@ -94,5 +97,22 @@ class TaskManagerTests: XCTestCase {
         sut.add(task: Task(title: "Foo"))
         
         XCTAssertEqual(sut.tasksCount, 1)
+    }
+    
+    func testSavedTaskShouldBeEqualWhenTaskManagerRecreated() {
+        var taskManager: TaskManager! = TaskManager()
+        let task = Task(title: "Foo")
+        let task1 = Task(title: "Bar")
+        taskManager.add(task: task)
+        taskManager.add(task: task1)
+        
+        NotificationCenter.default.post(name: UIApplication.willResignActiveNotification, object: nil)
+        
+        taskManager = nil
+        taskManager = TaskManager()
+        
+        XCTAssertEqual(taskManager.tasksCount, 2)
+        XCTAssertEqual(taskManager.task(at: 0), task)
+        XCTAssertEqual(taskManager.task(at: 1), task1)
     }
 }
